4e426c3bc31d188f526980d3ec17f74ade078e84
Austin <asmith@Austin.local>
2018-09-27 15:11:25 -0600

    updates

--- public/blog.php
+++ public/blog.php
@@ -1,5 +1,5 @@
-<?php include 'templates/header.php'; ?>
+<?php include '/templates/header.php'; ?>
 
 <h1>Blog</h1>
 
-<?php include 'templates/footer.php'; ?>
+<?php include '/templates/footer.php'; ?>
