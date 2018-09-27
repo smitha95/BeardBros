fbdccf17995df7b42a03bc1b00a472a9d9f57756
Austin <asmith@Austin.local>
2018-09-27 15:24:30 -0600

    updates

--- public/templates/header.php
+++ public/templates/header.php
@@ -36,7 +36,7 @@
            </ul>
 					 <ul class="nav navbar-nav navbar-right">
 					 	<li><a href="#">About</a></li>
-					 	<li><a href="/blog.php">Blog</a></li>
+					 	<li><a href="blog.php">Blog</a></li>
 					 	<li><a href="contact.php">Contact</a></li>
 					</ul>
 					<form class="navbar-form navbar-right">
