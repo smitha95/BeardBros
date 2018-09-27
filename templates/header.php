aef889b57c3efb5aa2e86bbd149f6bbf4b01159d
Austin <asmith@Austin.local>
2018-09-27 12:11:05 -0600

    updated contact page

--- templates/header.php
+++ templates/header.php
@@ -30,7 +30,7 @@
          <!-- Collect the nav links, forms, and other content for toggling -->
          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
            <ul class="nav navbar-nav">
-             <li class="active"><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
+             <li ><a href="index.php">Home</a></li>
              <li><a href="#">Products</a></li>
 						 <li><a href="#">Customers</a></li>
            </ul>
