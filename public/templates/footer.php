962bfc33a3be10bead1599891b3c429b31a84430
Austin <asmith@Austin.local>
2018-09-27 13:06:38 -0600

    updates

---
+++ public/templates/footer.php
@@ -0,0 +1,19 @@
+<!-- Script -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<footer>
+  <div class="navbar navbar-inverse navbar-fixed-bottom">
+    <div class="container">
+      <div class="navbar-collapse collapse" id="footer">
+
+        <i>BeardBros</i> &copy; <?php
+        $copyYear = 2018; // Set website start date
+        $curYear = date('Y'); // Keeps the second year updated
+        echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+        ?> Copyright.
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
---
+++ public/templates/header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Beard Bros</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<!-- Start of the top navigation bar -->
+     <nav class="navbar navbar-default navbar-inverse">
+       <div class="container-fluid">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <div class="navbar-header">
+           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+             <span class="sr-only">Toggle navigation</span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+           </button>
+           <!-- Brand image or name-->
+           <a class="navbar-brand" href="index.php">Beard Bros</a>
+         </div>
+
+         <!-- Collect the nav links, forms, and other content for toggling -->
+         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+           <ul class="nav navbar-nav">
+             <li ><a href="index.php">Home</a></li>
+             <li><a href="#">Products</a></li>
+						 <li><a href="#">Customers</a></li>
+           </ul>
+					 <ul class="nav navbar-nav navbar-right">
+					 	<li><a href="#">About</a></li>
+					 	<li><a href="#">Blog</a></li>
+					 	<li><a href="contact.php">Contact</a></li>
+					</ul>
+					<form class="navbar-form navbar-right">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Search">
+						</div>
+						<button type="submit" class="btn btn-default">Search</button>
+					</form>
+         </div><!-- /.navbar-collapse -->
+       </div><!-- /.container-fluid -->
+     </nav>
