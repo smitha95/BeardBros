1664d3424b9724f6058119e3f8abbf9702942928
Austin <asmith@Austin.local>
2018-09-27 08:21:35 -0600

    added header and footer

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>test</title>
-  </head>
-  <body>
-    <h1>test</h1>
-  </body>
-</html>
+<?php include '/templates/header.php'; ?>
+
+
+
+<?php include '/templates/header.php'; ?>
