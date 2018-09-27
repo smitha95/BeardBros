75dd18577f5cf8c341f045d3605773fba69d355f
Austin <asmith@Austin.local>
2018-09-27 16:05:37 -0600

    blog update

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php include 'templates/header.php'; ?>
 
 <h1>Home</h1>
-<p style="text-align:center; font-weight:bold;"> Welcome to the Beard Bros! <br/><br/>We are
+<p style="text-align:center; font-weight:bold; border: 2px #000000;"> Welcome to the Beard Bros! <br/><br/>We are
     two friends with a love for development.<br>
     My name is Austin and I take care of all <br>
     the front end development and I'm Isaac, <br>
