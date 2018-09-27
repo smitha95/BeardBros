07fc00d5495321748d2a73e30e00353a6601dc2e
Austin <asmith@Austin.local>
2018-09-27 15:54:27 -0600

    blog update

--- public/blog.php
+++ public/blog.php
@@ -1,7 +1,7 @@
 <?php include 'templates/header.php'; ?>
 
 <h1>Blog</h1>
-<p> Welcome to our blog page! Make sure to check back here regularly for updates
+<p style="text-align:center;"> Welcome to our blog page! Make sure to check back here regularly for updates
       on what we are doing!</p>
 
 <?php include 'templates/footer.php'; ?>
