2fa42689b77a001372e4033f01452c7350edb22e
Austin <asmith@Austin.local>
2018-09-28 07:47:47 -0600

    update

---
+++ public/about.php
@@ -0,0 +1,7 @@
+<?php include 'templates/header.php'; ?>
+
+<h1>About Us</h1>
+<p style="text-align:center; font-weight:bold;"> Welcome to our blog page! Make sure to check back here regularly for updates
+      on what we are doing!</p>
+
+<?php include 'templates/footer.php'; ?>
--- public/contact.php
+++ public/contact.php
@@ -1,5 +1,5 @@
 <?php include 'templates/header.php'; ?>
-
+<title>Contact</title>
 <h1>Contact</h1>
 
 <?php include 'templates/footer.php'; ?>
