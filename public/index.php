905d62471312827378ac05a1522aec566f2e8508
Austin <asmith@Austin.local>
2018-09-28 09:36:12 -0600

    update

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@
 <h1>Home</h1>
 <div class="center-block">
 
-<p class="home"style="text-align:center; font-weight:bold;"> Welcome to the Beard Bros! <br/><br/>We are
-    two friends with a love for development.<br>
-    My name is Austin and I take care of all <br>
-    the front end development and I'm Isaac, <br>
-    I take care of the back end development. <br>
+<p class="home"><span style="font-weight:bold;"> Welcome to the Beard Bros!</span> <br/><br/>We are
+    two friends with a love for development.
+    My name is Austin and I take care of all
+    the front end development and I'm Isaac,
+    I take care of the back end development.
     together we created the Beard Bros!</p>
 
 </div>
