ad0472a01f40a2d35c57f6bd15d7a6438e7a6f14
Austin <asmith@Austin.local>
2018-09-28 09:29:20 -0600

    update

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php include 'templates/header.php'; ?>
 
 <h1>Home</h1>
+<div class="center-block">
+
 <p class="home"style="text-align:center; font-weight:bold;"> Welcome to the Beard Bros! <br/><br/>We are
     two friends with a love for development.<br>
     My name is Austin and I take care of all <br>
@@ -8,6 +10,6 @@
     I take care of the back end development. <br>
     together we created the Beard Bros!</p>
 
-
+</div>
 
 <?php include 'templates/footer.php'; ?>
