c46257dd4f85ba675f7cdc25da60c71a179ddd25
Austin <asmith@Austin.local>
2018-09-28 07:54:22 -0600

    update

--- public/about.php
+++ public/about.php
@@ -1,5 +1,5 @@
 <?php include 'templates/header.php'; ?>
-
+<title>About Us</title>
 <h1>About Us</h1>
 <p style="text-align:center; font-weight:bold;"> Welcome to our blog page! Make sure to check back here regularly for updates
       on what we are doing!</p>
