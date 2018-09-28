5684ae36232fce26beaae823bbac79a0d4ece771
Austin <asmith@Austin.local>
2018-09-28 14:49:09 -0600

    update

--- public/index.php
+++ public/index.php
@@ -12,17 +12,17 @@
         </div>
       </div>
       <div class="col-md-4">
-        <h1><a href="about.php">About Us</a></h1>
+        <h1><a href="austin.php">Austin</a></h1>
         <div class="center-block">
           <p class="text-box">
-            <span style="font-weight:bold;">Austin:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur tortor quis augue faucibus tincidunt. Mauris pretium vitae nibh et pulvinar. Quisque vel vulputate sapien. Sed a odio et enim feugiat facilisis a ac elit. Pellentesque in lorem pulvinar, iaculis felis ut, mollis lorem. Nullam id dolor id orci cursus feugiat vel id purus. Aliquam sit amet quam in ligula viverra vulputate.</p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur tortor quis augue faucibus tincidunt. Mauris pretium vitae nibh et pulvinar. Quisque vel vulputate sapien. Sed a odio et enim feugiat facilisis a ac elit. Pellentesque in lorem pulvinar, iaculis felis ut, mollis lorem. Nullam id dolor id orci cursus feugiat vel id purus. Aliquam sit amet quam in ligula viverra vulputate.</p>
         </div>
       </div>
       <div class="col-md-4">
-        <h1><a href="about.php">About Us</a></h1>
+        <h1><a href="isaac.php">Isaac</a></h1>
         <div class="center-block">
             <p class="text-box">
-              <span style="font-weight:bold;">Isaac:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur tortor quis augue faucibus tincidunt. Mauris pretium vitae nibh et pulvinar. Quisque vel vulputate sapien. Sed a odio et enim feugiat facilisis a ac elit. Pellentesque in lorem pulvinar, iaculis felis ut, mollis lorem. Nullam id dolor id orci cursus feugiat vel id purus. Aliquam sit amet quam in ligula viverra vulputate.</p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur tortor quis augue faucibus tincidunt. Mauris pretium vitae nibh et pulvinar. Quisque vel vulputate sapien. Sed a odio et enim feugiat facilisis a ac elit. Pellentesque in lorem pulvinar, iaculis felis ut, mollis lorem. Nullam id dolor id orci cursus feugiat vel id purus. Aliquam sit amet quam in ligula viverra vulputate.</p>
 
         </div>
       </div>
