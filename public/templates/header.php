95c0189007ee5a2caeb7ca2af210481db5e4070b
Austin <asmith@Austin.local>
2018-09-28 14:05:11 -0600

    update

--- public/templates/header.php
+++ public/templates/header.php
@@ -9,7 +9,7 @@
 
 	<title>Beard Bros</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
