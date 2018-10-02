6449140cbce73dcab1c12c2f663a67eeda48e673
Austin <asmith@Austin.local>
2018-10-02 15:47:46 -0600

    update

--- public/index.php
+++ public/index.php
@@ -1,29 +1,27 @@
 <?php include 'templates/header.php'; ?>
+<div class="jumbotron jumbotron-fluid" style="background-color:#336B87 !important;">
+  <div class="container">
+    <h1 class="display-4">Welcome to the Beard Bros!</h1>
+    <p class="lead" style="text-align:center; font-size:23px;">We are two friends with a love for development together we created the Beard Bros!</p>
+  </div>
+</div>
 
 <div class="wrapper container">
   <div class="row">
-    <div class="col-md-4">
-      <h1>Welcome to the Beard Bros!</h1>
-      <div class="center-block">
-        <p class="text-box" style="font-weight:bold; font-size: 25px;">
-          We are two friends with a love for development together we created the Beard Bros!
-        </p>
-        </div>
-    </div>
-      <div class="col-md-4">
+      <div class="col-md-6">
         <h1><a href="austin.php">Austin</a></h1>
-        <div class="center-block">
+
           <p class="text-box"><span class="center_span">A little about me</span><br>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur tortor quis augue faucibus tincidunt. Mauris pretium vitae nibh et pulvinar. Quisque vel vulputate sapien. Sed a odio et enim feugiat facilisis a ac elit. Pellentesque in lorem pulvinar, iaculis felis ut, mollis lorem. Nullam id dolor id orci cursus feugiat vel id purus. Aliquam sit amet quam in ligula viverra vulputate.</p>
-        </div>
+
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6">
         <h1><a href="isaac.php">Isaac</a></h1>
-        <div class="center-block">
+
             <p class="text-box"><span class="center_span">A little about me</span><br>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur tortor quis augue faucibus tincidunt. Mauris pretium vitae nibh et pulvinar. Quisque vel vulputate sapien. Sed a odio et enim feugiat facilisis a ac elit. Pellentesque in lorem pulvinar, iaculis felis ut, mollis lorem. Nullam id dolor id orci cursus feugiat vel id purus. Aliquam sit amet quam in ligula viverra vulputate.</p>
 
-        </div>
+
       </div>
     </div>
 </div>
