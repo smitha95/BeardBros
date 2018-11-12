09486cc8f0a53a3797d1ce96bb1d35d0e12539ac
Austin <asmith@Austins-MacBook-Pro.local>
2018-11-11 20:02:25 -0600

    css fix

--- public/templates/header.php
+++ public/templates/header.php
@@ -4,7 +4,7 @@
 <head>
 	<!-- Add google anylitics to header before we go live -->
 	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta http-equiv="x-ua-compatible" >
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Beard Bros</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
