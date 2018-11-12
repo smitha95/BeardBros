c59a67b7e81c8f5589f8de04eb7143713297c263
Austin <asmith@Austins-MacBook-Pro.local>
2018-11-11 19:54:18 -0600

    fix css

--- public/templates/header.php
+++ public/templates/header.php
@@ -6,12 +6,12 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>Beard Bros</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
 
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+
 </head>
 
 <body>
